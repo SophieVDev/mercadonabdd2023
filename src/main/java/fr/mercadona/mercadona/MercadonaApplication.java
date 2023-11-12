a9f3a76f6536748cce7c5a65e5a37a414156abb2
SophieVDev <webdesigner@sitify.fr>
2023-11-12 14:37:14 +0100

    integration variable

--- src/main/java/fr/mercadona/mercadona/MercadonaApplication.java
+++ src/main/java/fr/mercadona/mercadona/MercadonaApplication.java
@@ -3,12 +3,12 @@ import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
 public class MercadonaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MercadonaApplication.class, args);
-	}
-
-}
+	}}
